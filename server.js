1c989a3d7de5ce525c8e464d29d7104fe19f1974
Ramnath <ramnath2609@gmail.com>
2020-07-15 23:11:41 +0530

    deploy

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const port = process.env.PORT || 5000
 if(process.env.NODE_ENV === 'production'){
     //set static folder
     app.use(express.static('client/build'))
-    app.use(expres.static(__dirname + './client/public'))
+    app.use(express.static(__dirname + './client/public'))
     app.get('*', (req, res)=>{
         res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'))
     })
